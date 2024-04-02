20f9ebdd7e2b2f215bc5c5eb2a1f86d5c64551b6
bjrmahmoud <mahmoud.bjr005@gmail.com>
2024-04-02 15:09:33 +0000

    first commit

---
+++ src/main/java/dao/DaoImpl.java
@@ -0,0 +1,17 @@
+package dao;
+
+import org.springframework.stereotype.Component;
+
+@Component("dao")
+
+public class DaoImpl implements IDao {
+    @Override
+    public double getData() {
+        /*
+        se connecter a la BD pour recuperer la temperature
+         */
+        System.out.println("Version base de donnes");
+        double temp=Math.random()*40;
+        return temp;
+    }
+}
